d1e38121c9a3b5009f86fce5848cd6fde103db1d
Patrick Martin <pjmartin@ycp.edu>
2013-05-28 12:20:19 -0400

    Finished examples on validators.

--- src/clojure_sandbox/core.clj
+++ src/clojure_sandbox/core.clj
@@ -19,3 +19,14 @@
 (reset! xfunc (fn [n] (* 3 n)))
 
 (@xfunc 5) ; outputs 15
+
+; Following the example on validators here...
+(def sarah (atom {:name "Sarah" :age 34}))
+; make the :age key required for a valid atom operation
+(set-validator! sarah #(or (:age %) 
+                              (throw (IllegalStateException. "You need to have an age!"))))
+; note this anonymous function passed as the validator function! what is passed into %? the atom?
+; Answer: Well, it appears that dissoc would get tested, then (:age @atom) is called and or'ed. The or will
+; fail, making the exception get thrown.
+(swap! sarah dissoc :age)
+
