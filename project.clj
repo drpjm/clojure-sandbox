31fdc849a25319de0a6a362acb90e1f0a11fc878
Patrick Martin <pjmartin@ycp.edu>
2013-07-02 16:07:21 -0400

    Started messing with core.async to imitate some functionality seen in my old java code.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.monads "0.1.4"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
                  [core.async "0.1.0-SNAPSHOT"]])
--- src/clojure_sandbox/async_testing.clj
+++ src/clojure_sandbox/async_testing.clj
@@ -1,3 +1,30 @@
 (ns clojure-sandbox.async-testing)
 
-(require '[clojure.core.async :as async])
+(require '[clojure.core.async :as async :refer :all])
+(import 'java.util.concurrent.ScheduledThreadPoolExecutor 'java.util.concurrent.TimeUnit 'java.util.concurrent.Executors)
+
+; making a scheduled thread pool...
+;(let [sched-pool (java.util.concurrent.ScheduledThreadPoolExecutor. 1)]
+;        (.scheduleAtFixedRate sched-pool (fn [] (println "Hello! from pool")) 0 1 TimeUnit/SECONDS))
+
+;(let [executor (Executors/newSingleThreadScheduledExecutor)]
+;        (.scheduleAtFixedRate executor (fn [] (println "Hello! from single thread")) 0 1 TimeUnit/SECONDS))
+
+(defn pub-msg
+        "This function sends a msg over the pub-chan"
+        [pub-chan]
+        (go (>! pub-chan "--Greetings--") ))
+
+(defn recv-msg
+        "This function reads a msg from sub-chan and capitalizes it!"
+        [sub-chan]
+        (println (.toUpperCase (<!! sub-chan))))
+
+(def test-chan (chan 10))
+(def receiver (future (while true (recv-msg test-chan) )))
+; this works!
+(>!! test-chan "awesomesauce")
+
+; and this too!
+(let [sched-pool (java.util.concurrent.ScheduledThreadPoolExecutor. 1)]
+        (.scheduleAtFixedRate sched-pool (fn [] (>!! test-chan "go go!")) 0 1 TimeUnit/SECONDS))
